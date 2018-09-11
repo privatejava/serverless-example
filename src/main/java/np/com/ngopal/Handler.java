b8998fff704ff0545ac99e3f7083100f980749e8
Narayan <privatejava@yahoo.com>
2018-09-11 16:52:41 +0545

    example of simple serverless java.

---
+++ src/main/java/np/com/ngopal/Handler.java
@@ -0,0 +1,46 @@
+package np.com.ngopal;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+
+	private static final Logger LOG = LogManager.getLogger(Handler.class);
+
+	@Override
+	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+		LOG.info("received: {}", input);
+		Response responseBody = new Response("Hello this is just a /foo [POST] response ", input);
+		return ApiGatewayResponse.builder()
+				.setStatusCode(200)
+				.setObjectBody(responseBody)
+				.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
+				.build();
+	}
+        
+        public ApiGatewayResponse barRequest(Map<String, Object> input, Context context) {
+		LOG.info("received: {}", input);
+		Response responseBody = new Response("Hello this is just a /bar response ", input);
+		return ApiGatewayResponse.builder()
+				.setStatusCode(200)
+				.setObjectBody(responseBody)
+				.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
+				.build();
+	}
+        
+        public ApiGatewayResponse infoRequest(Map<String, Object> input, Context context) {
+		LOG.info("received: {}", input);
+		Response responseBody = new Response("Hello this is just a /info response ", input);
+		return ApiGatewayResponse.builder()
+				.setStatusCode(200)
+				.setObjectBody(responseBody)
+				.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
+				.build();
+	}
+}
